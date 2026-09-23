a83e085ed0d20f756739b36895e5aafc8a38896f
Evilsmile1982 <derrikmaly1@gmail.com>
2026-09-23 23:37:06 +0200

    Downgrade targetSdk from 36 to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.zaero.fehlersuche"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
