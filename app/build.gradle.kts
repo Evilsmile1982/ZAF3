f19592d516dcedfe9e08f7d979b36fc4b804ecd0
Evilsmile1982 <derrikmaly1@gmail.com>
2026-09-23 23:03:47 +0200

    Downgrade compileSdk and targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.zaero.fehlersuche"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.zaero.fehlersuche"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
